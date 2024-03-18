b61fc9e578c98f870d834e9555fffaec26bfc04f
Damon <cman55245@gmail.com>
2024-03-18 19:57:06 +0530

    Added login and signup button

--- auth-nextjs/src/app/login/page.tsx
+++ auth-nextjs/src/app/login/page.tsx
@@ -1,10 +1,94 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+// import { axios } from "axios";
 
-const page = () => {
+
+export default function LogInPage() {
+
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const handleOnLogIn = async () => {};
   return (
-    <div className="flex bg-cyan-300 h-screen max-h-full w-1/2">
-        <h1 className="text-black text-2xl text-center">This is a Login page</h1>
+ 
+  <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-white " >
+    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img
+       className="mx-auto h-10 w-auto"
+        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+        alt="Your Company" />
+      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        Sign in to your account
+      </h2>
     </div>
+
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form className="space-y-6" >
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+            Email address
+          </label>
+          <div className="mt-2">
+            <input
+            placeholder="eg. theboynextdoor"
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              value={user.email}
+              className="block w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={(e)=>setUser({...user, email: e.target.value })}
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center justify-between">
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+              Password
+            </label>
+            <div className="text-sm">
+              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                Forgot password?
+              </a>
+            </div>
+          </div>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              value={user.password}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={(e)=>setUser({...user, password: e.target.value })}
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+           onClick={handleOnLogIn}
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+
+      <p className="mt-10 text-center text-md text-gray-500">
+        Not a member?{' '}
+        <Link href="/signup" className="text-blue-400 font-semibold"> Sign up</Link>
+      </p>
+    </div>
+  </div>
+
   )
 }
-
-export default page
--- auth-nextjs/src/app/signup/page.tsx
+++ auth-nextjs/src/app/signup/page.tsx
@@ -1,8 +1,70 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+// import { axios } from "axios";
 
-const page = () => {
+export default function SignUpPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const handleOnSignup = async () => {};
   return (
-    <div>This is a Signup page </div>
-  )
-}
+    <div className="bg-white flex flex-col items-center justify-center min-h-screen ">
+      <form className="relative space-y-3 rounded-xl bg-gray-200 p-6 shadow-xl lg:p-10 border border-gray-300 m-10 ">
+        <h1 className="text-xl text-black font-semibold lg:text-3xl">SignUp</h1>
 
-export default page
+        <div>
+          <label className="text-black "> Username </label>
+          <input
+            id="username"
+            type="text"
+            placeholder="eg. theboynextdoor "
+            className="mt-2 h-12 text-black w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
+            value={user.username}
+            onChange={(e) => setUser({...user, username: e.target.value})}
+          />
+        </div>
+        <div className="text-black">
+          <label className=""> Email Address </label>
+          <input
+            id="email"
+            type="email"
+            value={user.email}
+            placeholder="Info@example.com"
+            className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
+            onChange={(e)=>setUser({...user, email: e.target.value })}
+
+          />
+        </div>
+        <div>
+          <label className="text-black"> Password </label>
+          <input
+            id="password"
+            type="password"
+            className="mt-2 text-black h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
+            value={user.password}
+            onChange={(e) => setUser({...user , password: e.target.value})}
+          />
+        </div>
+
+        <div>
+          <button
+            type="button"
+            className="mt-5 w-full rounded-md bg-blue-600 p-2 text-center font-semibold text-white outline-none focus:ring"
+            onClick={handleOnSignup}
+          >
+           Sign Up
+          </button>
+        </div>
+        <div className="text-black flex items-center justify-center ">
+          <p>Alreay have an account? </p>
+          <Link href="/login"><p className="text-blue-400 cursor-pointer">Log In</p></Link>
+        </div>
+      </form>
+    </div>
+  );
+}
