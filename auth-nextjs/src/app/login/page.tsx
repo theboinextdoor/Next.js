4afbb4450cb3525ecc67b3b4f878fa6de4a9aa24
Damon <cman55245@gmail.com>
2024-03-18 16:33:40 +0530

    Project Setup and routes setup

---
+++ auth-nextjs/src/app/login/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div className="flex bg-cyan-300 h-screen max-h-full w-1/2">
+        <h1 className="text-black text-2xl text-center">This is a Login page</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ auth-nextjs/src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div>This is a Signup page </div>
+  )
+}
+
+export default page
