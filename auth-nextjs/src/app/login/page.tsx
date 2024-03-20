91b4429d9a0f179fba9020435491221663161aa7
Damon <cman55245@gmail.com>
2024-03-20 18:10:18 +0530

    added some SSR and CSR to the login page

--- auth-nextjs/src/app/login/page.tsx
+++ auth-nextjs/src/app/login/page.tsx
@@ -1,29 +1,67 @@
 "use client";
+
+
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
-// import { axios } from "axios";
+import axios  from "axios";
+import toast from "react-hot-toast";
 
 
 export default function LogInPage() {
+  const [showPassword, setShowPassword] = useState(false)
+  const router = useRouter()
+  const[buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading , setLoading] = useState(false)
 
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
 
-  const handleOnLogIn = async () => {};
+  useEffect(()=> {
+    if(user.email.length > 0 && user.password.length > 0){
+      setButtonDisabled(false)
+    }else{
+      setButtonDisabled(true)
+    }
+  }, [user])
+
+   const handleShowPassword = () => {
+    setShowPassword((prev) => !prev)
+   }
+
+
+  const handleOnLogIn = async () => {
+    try{
+      
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log(response.data)
+      toast.success("Login Successfull")
+      router.push("/profile")
+
+    }catch(error : any){
+
+      console.log(error.message)
+      toast.error(error.message)
+
+    }finally{
+      
+      setLoading(false)
+
+    }
+  };
   return (
  
   <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-white " >
     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img
+      {/* <img
        className="mx-auto h-10 w-auto"
         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-        alt="Your Company" />
+        alt="Your Company" /> */}
       <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-        Log in to your account
+      {loading ? "Processing" : "Log in to your account"}
       </h2>
     </div>
 
@@ -63,22 +101,24 @@ export default function LogInPage() {
             <input
               id="password"
               name="password"
-              type="password"
+              type={showPassword ? "text" : "password"}
               autoComplete="current-password"
               required
               value={user.password}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full rounded-md border-0 px-2 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               onChange={(e)=>setUser({...user, password: e.target.value })}
             />
+            <p className=" w-fit text-black text-base py-1 hover:text-red-400 cursor-pointer" onClick={handleShowPassword}>Show Password</p>
           </div>
         </div>
 
         <div>
           <button
+          type="button"
            onClick={handleOnLogIn}
             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Sign in
+          {buttonDisabled ? "Enter the detail" : "Login"}
           </button>
         </div>
       </form>
