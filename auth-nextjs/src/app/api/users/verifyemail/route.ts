a593ef1417f46b1edd7f28a6ebc62b9a98fa4a7a
Damon <cman55245@gmail.com>
2024-03-23 03:56:43 +0530

    added nodemailer

---
+++ auth-nextjs/src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,27 @@
+import {connect} from "@/dbConfig/dbConfig"
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel"
+
+
+connect();
+export async function POST(request : NextRequest){
+    try{
+
+        const reqBody = await request.json();
+        const {token} = reqBody;
+        console.log(token);
+
+        const user = await User.findOne({verifyToken : token,
+        verifyTokenExpiry : {$gt : Date.now()}})
+
+        if(!user){
+            return NextResponse.json(
+                {error : "Invalid token"} ,
+                {status : 400})
+        }
+
+    }
+    catch(error:any){
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
